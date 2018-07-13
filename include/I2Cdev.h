52342e054d53d6bab66695de96e6a22eea37aa18
duanzw <815240811@qq.com>
2018-07-13 15:32:58 +0800

    修改I2Cdev头文件错误

--- include/I2Cdev.h
+++ include/I2Cdev.h
@@ -31,6 +31,8 @@ public:
     static bool writeWord(uint8_t devAddr,uint8_t regAddr,uint16_t data);
     static bool writeBytes(uint8_t devAddr,uint8_t regAddr,uint8_t length,uint8_t *data);
     static bool writeWords(uint8_t devAddr,uint8_t regAddr,uint8_t length,uint16_t *data);
-    
+
     static uint16_t readTimeout;
-}
+};
+
+#endif
